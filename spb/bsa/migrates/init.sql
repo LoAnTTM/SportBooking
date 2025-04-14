bafafbab5cdb62e3b2de3ffad389c78dc3e66c15
LoAnTTM <loanttmgcd210320@fpt.edu.vn>
2025-04-15 03:02:03 +0700

    Remove tracking

--- spb/bsa/migrates/init.sql
+++
@@ -1,7 +0,0 @@
-CREATE TYPE public.status as ENUM ('active', 'inactive');
-
-CREATE TYPE public.platform as ENUM ('android', 'ios', 'inapp', 'email');
-
-CREATE TYPE public.progress as ENUM ('inprogress', 'pending', 'success', 'failure');
-
-CREATE TYPE public.oauth_provider as ENUM ('google', 'facebook');
